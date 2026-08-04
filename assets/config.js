e3d745ede3426eb8ab10c260b8b73e0a6620f02b
huleilei2 <huleilei2@mini1.cn>
2026-08-04 10:36:36 +0800

    Add Chrome Web Store install link

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 window.SiteConfig = Object.freeze({
     productName: 'Markdown to Image',
-    chromeWebStoreUrl: '',
+    chromeWebStoreUrl: 'https://chromewebstore.google.com/detail/markdown-to-image/oicohknclaabjbdcjhcddaklacoehlhi',
     supportEmail: 'huyaboxer@gmail.com'
 });
